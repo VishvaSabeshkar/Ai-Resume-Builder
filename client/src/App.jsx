9ba8528f8fbd48e71c91d6dd8de18a057bff27a3
Sabeshkar <sabeshkar3770@gmail.com>
2025-10-13 00:00:47 +0530

    Route done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
-function App() {
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home' 
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import ResumeBuilder from './pages/ResumeBuilder'
+import Preview from './pages/Preview'
+import Login from './pages/Login'
+ 
+const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path = '/' element = {<Home />}/>
+
+        <Route path='app' element = {<Layout/>}>
+          <Route index element = {<Dashboard/>}/>
+          <Route path='builder/:resumeId' element = {<ResumeBuilder />}/>
+        </Route>
+
+        <Route path = 'view/:resumeId' element = {<Preview/>}/>
+        <Route path = 'Login' element = {<Login/>}/>
+      </Routes>
     </div>
   )
 }
