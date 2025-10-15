729025056d25605f5c992c07a14b53ae288dafb0
Sabeshkar <sabeshkar3770@gmail.com>
2025-10-15 22:38:08 +0530

    adjustments done

--- client/src/components/Home/Title.jsx
+++ client/src/components/Home/Title.jsx
@@ -4,7 +4,7 @@ const Title = ({title, description}) => {
   return (
     <div className='text-center mt-6 text-slate-700'>
       <h2 className='text-3xl sm:text-4xl font-medium'>{title}</h2>
-      <p className='max-sm max-w-2xl mt-4 text-slate-500'>{description}</p>
+      <p className='mt-4 text-slate-500 max-w-2xl mx-auto'>{description}</p>
     </div>
   )
 }
